18678f3c30bc8ab2ff7a1978405044dcd77a237d
Nitesh <niteshjain132@users.noreply.github.com>
2018-11-27 10:06:01 +0530

    initial commit

---
+++ src/main/java/Attention-ReadMe.md
@@ -0,0 +1,7 @@
+## MathWorks Kids Game (Java)
+
+* Create a Java Project - you can name it MathWorks.
+* Copy Filter.java under ```src/main/java```
+* Copy FilterSpecs.java under ```src/test/java```
+* Add JUnit and <a href="https://mvnrepository.com/artifact/org.hamcrest/hamcrest-all/1.3">Hamcrest All Jar</a> to the classpath
+* MathWorks First story is already implemented.  Here are the story slides of <strong> <a href="//www.slideshare.net/DhavalDalal/math-works-kidsgame" title="MathWorks kids Game" target="_blank">MathWorks kids Game</a> </strong>
---
+++ src/test/java/FilterSpecs.java
@@ -0,0 +1,40 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.IsCollectionContaining.hasItem;
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
+
+public class FilterSpecs {
+    @Test
+    public void itSelectsPrimeNumbers() {
+        //Given
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
+
+        //When
+        List<Integer> primeNumbers = Filter.selectPrime(numbers);
+
+        //Then
+        assertThat(primeNumbers, hasSize(2));
+        assertThat(primeNumbers, hasItems(2, 3));
+        assertThat(primeNumbers, not(hasItems(1, 4)));
+    }
+
+    @Test
+    public void itDoesNotSelectNegativePrimeNumbers() {
+        //Given
+        List<Integer> numbers = Arrays.asList(-1, 2, -3, 4);
+
+        //When
+        List<Integer> primeNumbers = Filter.selectPrime(numbers);
+
+        //Then
+        assertThat(primeNumbers, hasSize(1));
+        assertThat(primeNumbers, hasItem(2));
+        assertThat(primeNumbers, not(hasItems(-1, -3, 4)));
+    }
+}
